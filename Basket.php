e7646898eb5f61297a1df6386d3969ba0094b822
Jordan Hall <jordan@hall05.co.uk>
2015-09-23 08:29:58 -0400

    Applied fixes from StyleCI

--- Basket.php
+++ Basket.php
@@ -7,102 +7,96 @@ class Basket
     public static function findCurrent()
     {
         $sessionStarted = @session_start();
-        
+
         if (!$sessionStarted) {
-            throw new \Exception("Unable to start session.");
+            throw new \Exception('Unable to start session.');
         }
-        
+
         if (!isset($_SESSION['ExiguousEcommerce'])) {
             $_SESSION['ExiguousEcommerce'] = new \stdClass();
         }
-        
+
         if (!isset($_SESSION['ExiguousEcommerce']->Basket) || !is_object($_SESSION['ExiguousEcommerce']->Basket)) {
-            $_SESSION['ExiguousEcommerce']->Basket = new Basket();
+            $_SESSION['ExiguousEcommerce']->Basket = new self();
         }
-        
+
         return $_SESSION['ExiguousEcommerce']->Basket;
     }
-    
-    public $items = array();
-    
+
+    public $items = [];
+
     public function __construct()
     {
-        
     }
-    
+
     public function addProduct($product, $quantity = 1)
     {
         if (!$product || !is_object($product) || !isset($product->id)) {
-            throw new \Exception("Unable to add an invalid product to the basket.");
+            throw new \Exception('Unable to add an invalid product to the basket.');
         }
-        
-        if ($quantity<=0) {
-            throw new \Exception("Unable to add <= 0 quantity of a product to the basket.");
+
+        if ($quantity <= 0) {
+            throw new \Exception('Unable to add <= 0 quantity of a product to the basket.');
         }
-        
+
         foreach ($this->items as $item) {
-            
-            if ($item->product->id==$product->id) {
-                
+            if ($item->product->id == $product->id) {
                 $item->quantity += $quantity;
                 $this->save();
+
                 return;
             }
         }
-        
+
         $this->items[] = new BasketItem($product, $quantity);
         $this->save();
     }
-    
+
     public function removeProduct($product)
     {
         if (!$product || !is_object($product) || !isset($product->id)) {
-            throw new \Exception("Unable to add an invalid product to the basket.");
+            throw new \Exception('Unable to add an invalid product to the basket.');
         }
-        
+
         foreach ($this->items as $key => $item) {
-            
-            if ($item->product->id==$product->id) {
-                
+            if ($item->product->id == $product->id) {
                 unset($this->items[$key]);
-                
+
                 $this->save();
+
                 return;
             }
         }
     }
-    
+
     public function offsetProductQuantity($product, $quantityOffset)
     {
         if (!$product || !is_object($product) || !isset($product->id)) {
-            throw new \Exception("Unable to add an invalid product to the basket.");
+            throw new \Exception('Unable to add an invalid product to the basket.');
         }
-        
+
         foreach ($this->items as $item) {
-            
-            if ($item->product->id==$product->id) {
-                
+            if ($item->product->id == $product->id) {
                 if ($item->quantity + $quantityOffset < 0) {
-                    throw new \Exception("Unable to offset quantity of a product to the basket to below zero.");
+                    throw new \Exception('Unable to offset quantity of a product to the basket to below zero.');
                 }
-                
+
                 $item->quantity += $quantityOffset;
                 $this->save();
+
                 return;
             }
         }
     }
-    
+
     private function save()
     {
         $sessionStarted = @session_start();
-        
+
         if (!$sessionStarted) {
-            throw new \Exception("Unable to start session.");
+            throw new \Exception('Unable to start session.');
         }
-        
+
         $_SESSION['ExiguousEcommerce']->Basket = $this;
     }
 }
-
-?>
--- Category.php
+++ Category.php
@@ -4,44 +4,42 @@ namespace DivineOmega\ExiguousEcommerce;
 
 class Category
 {
-    public static $directory = ".categories";
-    public static $class = "Category";
-    
+    public static $directory = '.categories';
+    public static $class = 'Category';
+
     public static function find($id)
     {
         return ExiguousEcommerceItem::find(self::$directory, self::$class, $id);
     }
-    
+
     public static function findBySlug($slug)
     {
         return ExiguousEcommerceItem::findBySlug(self::$directory, self::$class, $slug);
     }
-    
+
     public $id = null;
     public $data = null;
-    
+
     public function __construct($id, $data)
     {
         $this->id = $id;
         $this->data = $data;
     }
-    
+
     public function products()
     {
         $ids = isset($this->data->product_ids) && is_array($this->data->product_ids) ? $this->data->product_ids : [];
-        
+
         $objs = [];
-        
-        foreach($ids as $id) {
-            $obj = Product::find($id); 
-            
+
+        foreach ($ids as $id) {
+            $obj = Product::find($id);
+
             if ($obj) {
                 $objs[] = $obj;
             }
         }
-        
+
         return $objs;
     }
 }
-
-?>
