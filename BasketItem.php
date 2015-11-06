b35c5ece4b69f4093d176b0de5a32c25d24a47a6
Jordan Hall <jordan@hall05.co.uk>
2015-11-06 12:14:21 -0500

    Applied fixes from StyleCI

--- BasketItem.php
+++ BasketItem.php
@@ -12,13 +12,13 @@ class BasketItem
         $this->product = $product;
         $this->quantity = $quantity;
     }
-    
+
     public function unitCost()
     {
         // TODO: Retrieve current unit cost based on priceRules
         return 0.00;
     }
-    
+
     public function lineTotal()
     {
         return $this->unitCost() * $this->quantity;
