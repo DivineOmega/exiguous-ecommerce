f41f94246efefdd7e0c4e42a8166128128a707ce
Jordan Hall <jordan@hall05.co.uk>
2016-05-09 06:50:08 -0400

    Applied fixes from StyleCI

--- src/BasketItem.php
+++ src/BasketItem.php
@@ -21,28 +21,27 @@ class BasketItem
 
           // Check product price rules for a price.
           if (isset($this->product->data->priceRules) && is_array($this->product->data->priceRules)) {
-            foreach($this->product->data->priceRules as $priceRule) {
-              if ($priceRule->currency != $currency) {
-                continue;
+              foreach ($this->product->data->priceRules as $priceRule) {
+                  if ($priceRule->currency != $currency) {
+                      continue;
+                  }
+                  if ($this->quantity > $priceRule->greaterThanXUnits) {
+                      $unitCost = $priceRule->value;
+                  }
               }
-              if ($this->quantity > $priceRule->greaterThanXUnits) {
-                $unitCost = $priceRule->value;
-              }
-            }
           }
 
           // If we've not got a unit cost from checking the price rules, then
           // just get the regular product price.
           if (!$unitCost) {
-            foreach ($this->product->data->prices as $price) {
-              if ($price->currency != $currency) {
-                continue;
+              foreach ($this->product->data->prices as $price) {
+                  if ($price->currency != $currency) {
+                      continue;
+                  }
+                  $unitCost = $price->value;
+                  break;
               }
-              $unitCost = $price->value;
-              break;
-            }
           }
-
         }
 
         return $unitCost;
